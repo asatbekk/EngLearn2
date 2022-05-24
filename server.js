a9f1fe25d5551ffe3b576088951da98fda6e1721
asatbekk <asatcom26@gmail.com>
2022-05-24 19:29:06 +0600

    ass4

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app = express()
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({extended:true}))
+const port = 3000;
 const fs = require('fs')
 const path = require('path')
-const cool = require('cool-ascii-faces')
-const mongodb = require('mongodb')
-const {MongoClient} = require('mongodb')
-
-const client = new MongoClient('mongodb+srv://asat2003080808:asat2003080808@cluster0.yt7bp.mongodb.net/Engliear202020?retryWrites=true&w=majority')
-
-
-const mongoClient = new mongodb.MongoClient('mongodb://localhost:5000', {
-    useUnifiedTopology: true
-});
+const user = require("./model/user");
+mongoose.connect("mongodb+srv://asatbekkkk:asat20030808@cluster0.povlf.mongodb.net/ENGLEARN20",(err)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log('connected');
+    }
+})
 
 app.use('/static', express.static(__dirname+'/public'))
-
 
 
 app.get('/vocabulary',(req, res) => {
@@ -39,30 +40,41 @@ app.get('/lecture',(req, res) => {
 app.get('/registration',(req, res) => {
     res.sendFile(__dirname+'/registration.html')
 })
-app.get('/Grammar',(req, res) => {
+app.get('/grammar',(req, res) => {
     res.sendFile(__dirname+'/Grammar.html')
 })
 
 
-
-const start = async () => {
-    try {
-        await client.connect()
-        const db = client.db('users')
-        const coll = db.collection('userdata')
-
-
-        res = await coll.insertOne({name:"Asatbekk"})
-
-        app.listen(PORT, () => console.log('Server works ') )
-        console.log(res)
+app.post('/',(req,res)=>{
+    const newUser = new user({
+        email:req.body.email,
+        password:req.body.password,
+    })
+    if(req.body.password === req.body.password) {
+        newUser.save((err) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log("Ok")
+            }
+        })
+        res.json({msg:"user saved"})
     }
-    catch (e) {
-        console.log(e)
+    else{
+        res.json({msg:"err"})
     }
-}
 
-start()
+
+})
+
+
+
+
+
+app.listen(port, () =>
+    console.log(`App listening at http://localhost:${port}`)
+);
+
 
 
 
